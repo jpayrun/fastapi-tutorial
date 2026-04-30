001e83c519965fa11b52f6262f5cd762204423b9
jlpayson <27322284+jpayrun@users.noreply.github.com>
2026-04-30 17:51:39 -0400

    Added delete

--- main.py
+++ main.py
@@ -127,6 +127,17 @@ def update_post_partial(post_id: int,
     db.refresh(post)
     return post
 
+@app.delete("/api/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_posts(post_id: int, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalars().first()
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    
+    db.delete(post)
+    db.commit()
+
 @app.post(
     "/api/users",
     response_model=UserResponse,
