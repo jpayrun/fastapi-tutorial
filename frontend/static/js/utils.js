b4173438eb037d9042ee2afd2f622c959b2d6d05
jlpayson <27322284+jpayrun@users.noreply.github.com>
2026-05-02 14:05:28 -0400

    Adding modals for form input

---
+++ frontend/static/js/utils.js
@@ -0,0 +1,24 @@
+
+export function getErrorMessage(error){
+    if (typeof error.detail === "string") {
+        return error.detail
+    } else if (Array.isArray(error.detail)) {
+        return error.detail.map((err) => err.msg).join(". ");
+    }
+    return "An error occurred. Please try again.";
+};
+
+export function showModal(modalId) {
+    const modal = bootstrap.Modal.getOrCreateInstance(
+        document.getElementById(modalId)
+    );
+    modal.show();
+    return modal;
+}
+
+export function hideModel(modelId) {
+    const modal = bootstrap.Modal.getInstance(
+        document.getElementById(modalId)
+    );
+    if (modal) modal.hide();
+}
